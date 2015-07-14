2e27f230531472cab37670f18b70d7cc9d7f6334
H3roSav3r <zachery.matteson@gmail.com>
2015-07-14 13:42:23 -0700

    Signed-off-by: H3roSav3r <zachery.matteson@gmail.com>

---
+++ Lab/Lab_14/Template/main.cpp
@@ -0,0 +1,55 @@
+/* 
+ * File:   main.cpp
+ * Author: Zachery Matteson
+ * Purpose:
+ * Created on July 14, 2015, 12:12 PM
+ */
+
+//System Libraries
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+int example(int, int&);
+
+//Execution
+int main(int argc, char** argv) {
+    
+    //Declare Variables
+    
+    //Initialize and Prompt for inputs
+    
+    //Process the inputs
+    
+    //Output the results
+    
+    //Exit
+    return 0;
+}
+/*******************************************
+ ************ Example Function **************
+ ********************************************
+ * Purpose: Show how to comment a function
+ * Input:
+ *      a-> Example input pass by value
+ * Input-Output:
+ *      b-> Example input-output pass by reference
+ * Output:
+ *      c-> 
+ *******************************************/
+
+int example(int a, int&b){
+    //Declare a variable c
+    int c;
+    
+    //Process
+    
+    //Output
+    return c;
+}
